05f061f45bd6ae64ec1fb6757962d9e623571159
Artur <arturdiniz06@gmail.com>
2025-11-27 15:48:32 -0300

    adicionando imagem ao User

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -170,7 +170,10 @@ module.exports = class UserController {
 
         const { name, email, phone, password, confirmpassword } = req.body
 
-        let image = ""
+
+        if (req.file) {
+            user.image = req.file.filename
+        }
 
         //validação
         if (!name) {
