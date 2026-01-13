0c7c15f03deb3f52e5c0187e4204553561089306
Artur <arturdiniz06@gmail.com>
2026-01-13 15:19:48 -0300

    Fazendo integração com a API

--- 15_GET_A_PET/frontend/src/hooks/useAuth.js
+++ 15_GET_A_PET/frontend/src/hooks/useAuth.js
@@ -2,10 +2,16 @@ import api from '../utils/api'
 
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import userFlashMessage from './useFlashMessage'
 
 export default function useAuth() {
+    const{setFlashMessage} = userFlashMessage()
 
     async function register(user) {
+
+        let msgText = 'Cadastro realizado com sucesso'
+        let msgType = 'sucess'
+
         try {
             const data = await api.post('/users/register', user).then((Response) => {
                 return Response.data
@@ -13,11 +19,13 @@ export default function useAuth() {
             console.log(data)
 
         } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
             console.log(error)
         }
 
-
-
+        setFlashMessage(msgText,msgType)
     }
+
     return { register }
 }
